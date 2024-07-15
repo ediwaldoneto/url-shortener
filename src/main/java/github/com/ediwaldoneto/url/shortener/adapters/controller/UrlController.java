320d573b503ddde8f533327248cb3f7b48b005b0
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2024-07-14 21:05:00 -0300

    ajuste import

--- src/main/java/github/com/ediwaldoneto/url/shortener/adapters/controller/UrlController.java
+++ src/main/java/github/com/ediwaldoneto/url/shortener/adapters/controller/UrlController.java
@@ -5,7 +5,13 @@ import github.com.ediwaldoneto.url.shortener.application.dto.UrlResponse;
 import github.com.ediwaldoneto.url.shortener.application.service.UrlService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/shorten-url")
